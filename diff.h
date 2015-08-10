ad9fd89f2f24333b6d80dfa8cb6e9801f33eb421
Brian Granzow <brian.granzow@gmail.com>
2015-08-10 14:10:15 -0400

    add binary multiplication and division

--- diff.h
+++ diff.h
@@ -189,6 +189,70 @@ AD<N> operator-(AD<N> const& L, AD<N> const& R)
   tmp.val() = L.val() - R.val();
 }
 
+/** \brief binary multiplication between a double and an AD variable */
+template <unsigned int N>
+AD<N> operator*(double L, AD<N> const& R)
+{
+  AD<N> tmp;
+  for (unsigned int i=0; i < N; ++i)
+    tmp.dx(i) = L * R.dx(i);
+  tmp.val() = L * R.val();
+  return tmp;
+}
+
+/** \brief binary multiplication between an AD variable and a double */
+template <unsigned int N>
+AD<N> operator*(AD<N> const& L, double R)
+{
+  AD<N> tmp;
+  for (unsigned int i=0; i < N; ++i)
+    tmp.dx(i) = L.dx(i) * R;
+  tmp.val() = L.val() * R;
+  return tmp;
+}
+
+/** \brief binary multiplication between two AD variables */
+template <unsigned int N>
+AD<N> operator*(AD<N> const& L, AD<N> const& R)
+{
+  AD<N> tmp;
+  for (unsigned int i=0; i < N; ++i)
+    tmp.dx(i) = L.dx(i) * R.val() + L.val() * R.dx(i);
+  tmp.val() = L.val() * R.val();
+  return tmp;
+}
+
+/** \brief binary division between a double and an AD variable */
+template <unsigned int N>
+AD<N> operator/(double L, AD<N> const& R)
+{
+  AD<N> tmp;
+  for (unsigned int i=0; i < N; ++i)
+    tmp.dx(i) = ( -L*R.dx(i) ) / (R.val()*R.val());
+  tmp.val() = L / R.val();
+  return tmp;
+}
+
+/** \brief binary division between an AD variable and a double */
+template <unsigned int N>
+AD<N> operator/(AD<N> const& L, double R)
+{
+  AD<N> tmp;
+  for (unsigned int i=0; i < N; ++i)
+    tmp.dx(i) = L.dx(i) / R;
+  tmp.val() = L.val() / R;
+}
+
+/** \brief binary division between two AD variables */
+template <unsigned int N>
+AD<N> operator/(AD<N> const& L, AD<N> const& R)
+{
+  AD<N> tmp;
+  for (unsigned int i=0; i < N; ++i)
+    tmp.dx(i) = (L.dx(i) * R.val() - L.val() * R.dx(i) ) / (R.val() * R.val());
+  return tmp;
+}
+
 }
 
 #endif
